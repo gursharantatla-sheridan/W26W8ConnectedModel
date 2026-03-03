9ec12685ea7c22aa37afae67d8a532fdf7872b81
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-03 18:04:48 -0500

    load data

--- W26W8ConnectedModel/MainWindow.xaml.cs
+++ W26W8ConnectedModel/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace W26W8ConnectedModel
 {
@@ -16,9 +19,35 @@ namespace W26W8ConnectedModel
     /// </summary>
     public partial class MainWindow : Window
     {
+        // read the conn str from App.config
+        string connStr = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+            string query = "select EmployeeID, FirstName, LastName, City, Country from Employees";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataTable tbl = new DataTable();
+            tbl.Load(reader);
+            grdEmployees.ItemsSource = tbl.DefaultView;
+
+            conn.Close();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
         }
     }
 }

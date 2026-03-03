6f6dd564c7ddd98444bd90b5ed3d309164094461
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-03 18:40:58 -0500

    cmd params

--- W26W8ConnectedModel/MainWindow.xaml.cs
+++ W26W8ConnectedModel/MainWindow.xaml.cs
@@ -29,25 +29,42 @@ namespace W26W8ConnectedModel
 
         private void LoadData()
         {
-            SqlConnection conn = new SqlConnection(connStr);
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "select EmployeeID, FirstName, LastName, City, Country from Employees";
+                SqlCommand cmd = new SqlCommand(query, conn);
 
-            string query = "select EmployeeID, FirstName, LastName, City, Country from Employees";
-            SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
 
-            conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
 
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            DataTable tbl = new DataTable();
-            tbl.Load(reader);
-            grdEmployees.ItemsSource = tbl.DefaultView;
-
-            conn.Close();
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
+            //conn.Close();
         }
 
         private void btnLoad_Click(object sender, RoutedEventArgs e)
         {
             LoadData();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "select EmployeeID, FirstName, LastName, City, Country from Employees where FirstName='" + txtFirstname.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
         }
     }
 }

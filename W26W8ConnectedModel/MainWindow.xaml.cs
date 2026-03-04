5eebc1e84fd5f8c7dbe26da97f7707af1eada45a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-03 19:03:35 -0500

    insert

--- W26W8ConnectedModel/MainWindow.xaml.cs
+++ W26W8ConnectedModel/MainWindow.xaml.cs
@@ -86,5 +86,25 @@ namespace W26W8ConnectedModel
                 MessageBox.Show("Total rows = " + numOfRows);
             }
         }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "insert into Employees(FirstName, LastName) values (@fn, @ln)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("fn", txtFirstname.Text);
+                cmd.Parameters.AddWithValue("ln", txtLastname.Text);
+
+                conn.Open();
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result == 1)
+                    MessageBox.Show("New employee added");
+                else
+                    MessageBox.Show("New employee not added");
+            }
+        }
     }
 }
